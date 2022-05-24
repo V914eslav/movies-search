545b068051d8d1a258fc53be6992ed4ca0ac1b4b
V914eslav <v914-working@rambler.ru>
2022-05-24 17:51:06 +0300

    add more inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,6 +6,10 @@ class Form extends Component {
     this.state = {
       firstName: "",
       email: "",
+      message: "",
+      select: "",
+      subscription: false,
+      gender: "",
     };
   }
 
@@ -14,8 +18,15 @@ class Form extends Component {
       [e.target.name]: e.target.value,
     });
   };
+
+  handleCheckedChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.checked,
+    });
+  };
   render() {
-    const { firstName, email } = this.state;
+    const { firstName, email, message, select, subscription, gender } =
+      this.state;
     return (
       <div>
         <input
@@ -32,6 +43,44 @@ class Form extends Component {
           value={email}
           onChange={this.handleChange}
         />
+        <textarea
+          name="message"
+          placeholder="message"
+          value={message}
+          onChange={this.handleChange}
+        />
+        <select name="select" value={select} onChange={this.handleChange}>
+          <option value="" disabled></option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        <label>
+          <input
+            type="checkbox"
+            name="subscription"
+            checked={subscription}
+            onChange={this.handleCheckedChange}
+          />
+          subscription
+        </label>
+        <br />
+        <input
+          type="radio"
+          name="gender"
+          value="male"
+          onChange={this.handleChange}
+          checked={gender === "male"}
+        />
+        Male
+        <input
+          type="radio"
+          name="gender"
+          value="female"
+          onChange={this.handleChange}
+          checked={gender === "female"}
+        />
+        Female
       </div>
     );
   }
