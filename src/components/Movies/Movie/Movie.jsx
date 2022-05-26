f42f36cae7471dfdd5346684544549e5753e8322
V914eslav <v914-working@rambler.ru>
2022-05-26 18:01:21 +0300

    add styles from Movies

--- src/components/Movies/Movie/Movie.jsx
+++ src/components/Movies/Movie/Movie.jsx
@@ -5,14 +5,23 @@ import cn from "classnames";
 
 const Movie = (props) => {
   const { Title, Year, imdbID, Type, Poster } = props;
-  const arr = props;
-  // console.log(arr);
-  // console.log(arr[0]);
 
   return (
     <div className={cn("card", styles.movie)} id={imdbID}>
       <div className="card-image waves-effect waves-block waves-light">
-        <img className="activator" src={Poster} alt={Title} />
+        {Poster === "N/A" ? (
+          <img
+            className={cn("activator", styles.img)}
+            src={`https://via.placeholder.com/300x400?text=${Title}`}
+            alt={Title}
+          />
+        ) : (
+          <img
+            className={cn("activator", styles.img)}
+            src={Poster}
+            alt={Title}
+          />
+        )}
       </div>
       <div className="card-content">
         <span className="card-title activator grey-text text-darken-4">
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Movie from "./Movie/Movie";
 
+import styles from "./Movies.module.css";
 export default class Movies extends Component {
   render() {
     const { movies } = this.props;
     console.log(this.props);
     return (
-      <div className="movies">
+      <div className={styles.movies}>
         {movies.map((movie) => (
           <Movie key={movie.imdbID} {...movie} />
         ))}
